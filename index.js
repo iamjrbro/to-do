4e2fb3a6edadc0f6aeda6600fdc0f26cc416bd52
Julia Ribeiro <jrstudin@gmail.com>
2023-08-16 18:14:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -57,3 +57,52 @@ function newElement() {
     }
   }
 }
+
+
+//creating a new list item when clicking a new element
+document.getElementById('firstInput').addEventListener('keyup', function(event){
+  if(event.key === 'Enter'){
+    newElement();
+  }
+});
+
+
+//getting date and time
+
+
+/*In the above code, I've added a new function getCurrentDateAndTime that returns the current date and time in a formatted string. I've modified the newElement function to use this formatted date and time along with the user's input when creating a new list item. Additionally, I've added an event listener to the input field to trigger the newElement function when the Enter key is pressed.
+
+Please note that this code assumes you have an HTML input element with the ID "firstInput" for the user to enter new items. Also, make sure you have an element with the ID "ul" for appending the new list items. You may need to adjust the IDs and HTML structure accordingly.*/
+
+function getDataandTime(){
+  var now = new Date();
+  var options = {weekday: 'long', year: 'numeric', month:'long', day: 'numeric', hour:'2-digit', minute:'2-digit',};
+  return now.toLocaleDateString('pt-BR', options)
+}
+
+
+//saving it to the memorie
+function saveInfo(item){
+  var saveItens = JSON.parse(localStorage.getItem('listItems')) || [];
+  savedItens.push(item);
+  localStorage.setItem('listItens', JSON.stringify(savedItens));
+}
+
+
+//loading all saved itens from the store page when it loads
+window.onload = function(){
+  var saveItens = JSON.parse(localStorage.getItem('listItens')) || [];
+  var ul = document.getElementById('ul')
+  for (var i = 0; i < saveItens.length; i++){
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(saveItens[i]));
+    ul.appendChild(li);
+  }
+}
+
+
+
+
+/*In this code, the saveToList function takes care of adding a new item to the local storage. The window.onload function is used to load the saved items from local storage and populate the list when the page loads.
+
+Remember to adjust your HTML structure accordingly and ensure that you have an element with the ID "ul" for appending the list items.*/
