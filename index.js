a9a440b1cd8960bff34c70b03aa6f03a3f2f1501
Julia Ribeiro <jrstudin@gmail.com>
2023-08-17 11:13:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // creating the close button and append it to each list item
 var list = document.getElementsByTagName("LI");
-var l;
-for (l = 0; l < list.length; l++) {
+for (var l = 0; l < list.length; l++) {
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
   span.className = "close";
@@ -9,19 +8,16 @@ for (l = 0; l < list.length; l++) {
   list[l].appendChild(span);
 }
 
-
-// clicking on a close button to hide the current list item
+// Clicking on a close button to hide the current list item
 var close = document.getElementsByClassName("close");
-var l;
-for (l = 0; l < close.length; l++) {
+for (var l = 0; l < close.length; l++) {
   close[l].onclick = function() {
     var div = this.parentElement;
     div.style.display = "none";
-  }
+  };
 }
 
-
-// adding a checked symbol when clicking on a list item
+// Adding a checked symbol when clicking on a list item
 var list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
   if (ev.target.tagName === 'LI') {
@@ -29,10 +25,33 @@ list.addEventListener('click', function(ev) {
   }
 }, false);
 
+// Getting date and time
+function getDataAndTime() {
+  var now = new Date();
+  var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+  return now.toLocaleDateString('pt-BR', options);
+}
 
-// creating a new list item when clicking on the "add" button
+// Saving item to local storage
+function saveInfo(item) {
+  var savedItems = JSON.parse(localStorage.getItem('listItems')) || [];
+  savedItems.push(item);
+  localStorage.setItem('listItems', JSON.stringify(savedItems));
+}
+
+// Loading all saved items from local storage when the page loads
+window.onload = function() {
+  var savedItems = JSON.parse(localStorage.getItem('listItems')) || [];
+  var ul = document.getElementById('ul');
+  for (var i = 0; i < savedItems.length; i++) {
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(savedItems[i]));
+    ul.appendChild(li);
+  }
+};
+
+// Creating a new list item when clicking on the "add" button or pressing Enter
 function newElement() {
-
   var li = document.createElement("li");
   var inputValue = document.getElementById("firstInput").value;
   var t = document.createTextNode(inputValue);
@@ -41,8 +60,9 @@ function newElement() {
     alert("You must write something!");
   } else {
     document.getElementById("ul").appendChild(li);
+    saveInfo(inputValue); // Save the new item to local storage
   }
-  document.getElementById("ul").value = "";
+  document.getElementById("firstInput").value = "";
 
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
@@ -50,46 +70,15 @@ function newElement() {
   span.appendChild(txt);
   li.appendChild(span);
 
-  for (l = 0; l < close.length; l++) {
-    close[l].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
+  span.onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  };
 }
 
-//creating a new list item when clicking a new element
-document.getElementById('firstInput').addEventListener('keyup', function(event){
-  if(event.key === 'Enter'){
+// Creating a new list item when pressing Enter in the input field
+document.getElementById('firstInput').addEventListener('keyup', function(event) {
+  if (event.key === 'Enter') {
     newElement();
   }
 });
-
-
-//getting date and time
-function getDataandTime(){
-  var now = new Date();
-  var options = {weekday: 'long', year: 'numeric', month:'long', day: 'numeric', hour:'2-digit', minute:'2-digit',};
-  return now.toLocaleDateString('pt-BR', options)
-}
-
-
-//saving it to the memorie
-function saveInfo(item){
-  var saveItens = JSON.parse(localStorage.getItem('listItems')) || [];
-  saveItens.push(item);
-  localStorage.setItem('listItens', JSON.stringify(savedItens));
-}
-
-
-//loading all saved itens from the store page when it loads
-window.onload = function(){
-  var saveItens = JSON.parse(localStorage.getItem('listItens')) || [];
-  var ul = document.getElementById('ul')
-  for (var l = 0; l < saveItens.length; l++){
-    var li = document.createElement('li');
-    li.appendChild(document.createTextNode(saveItens[i]));
-    ul.appendChild(li);
-  }
-}
-
